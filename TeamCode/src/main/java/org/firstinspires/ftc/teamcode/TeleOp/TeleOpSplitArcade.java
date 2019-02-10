76fbd782cba0489077e3be61c32a62a688a2277b
abhitirumala <abhiram2001.tv@gmail.com>
2019-02-09 20:09:31 -0500

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpSplitArcade.java
@@ -0,0 +1,225 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.*;
+import com.qualcomm.robotcore.hardware.*;
+import com.qualcomm.robotcore.util.*;
+
+/**
+ * This file contains an example of an iterative (Non-Linear) "OpMode".
+ * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+ * The names of OpModes appear on the menu of the FTC Driver Station.
+ * When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all iterative OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="5518 TeleOp v4.1", group="TeleOp")
+//@Disabled
+public class TeleOpSplitArcade extends OpMode
+{
+    // Declare OpMode members.
+    private ElapsedTime runtime      = new ElapsedTime();
+
+    private DcMotor leftFrontDrive   = null;
+    private DcMotor rightFrontDrive  = null;
+    private DcMotor leftBackDrive    = null;
+    private DcMotor rightBackDrive   = null;
+    private DcMotor collector        = null;
+    private DcMotor spool            = null;
+    private DcMotor arm              = null;
+    private DcMotor winch            = null;
+
+    private Servo collectorWall      = null;
+
+
+    private boolean isOnA            = false;
+    private double slowModeValue     = 1.0;
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init()
+    {
+
+        // Initialize the hardware variables
+        leftFrontDrive      = hardwareMap.get(DcMotor.class, "1");
+        rightFrontDrive     = hardwareMap.get(DcMotor.class, "2");
+        leftBackDrive       = hardwareMap.get(DcMotor.class, "3");
+        rightBackDrive      = hardwareMap.get(DcMotor.class, "0");
+
+        collector           = hardwareMap.get(DcMotor.class, "collector");
+        spool               = hardwareMap.get(DcMotor.class, "spool");
+        arm                 = hardwareMap.get(DcMotor.class, "arm");
+        winch               = hardwareMap.get(DcMotor.class, "winch");
+
+        collectorWall       = hardwareMap.get(Servo.class, "collector wall");
+
+        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        collector.setDirection(DcMotor.Direction.FORWARD);
+        arm.setDirection(DcMotor.Direction.FORWARD);
+        spool.setDirection(DcMotor.Direction.FORWARD);
+        winch.setDirection(DcMotor.Direction.FORWARD);
+
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        spool.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //spool.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+
+        // Tell the driver that initialization is complete.
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop()
+    {
+
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start()
+    {
+        runtime.reset();
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop()
+    {
+        // Setup a variable for each drive wheel to save power level for telemetry
+
+
+
+        if(gamepad1.a && !isOnA) {
+            if(slowModeValue == 1.0)
+                slowModeValue = 0.5;
+            else
+                slowModeValue = 1.0;
+            isOnA = true;
+        }
+        else if(!gamepad1.a)
+            isOnA = false;
+
+        if(gamepad2.right_bumper)
+        {
+            collector.setPower(0.5);
+        }
+        else if (gamepad2.left_bumper)
+        {
+            collector.setPower(-0.5);
+        }
+        else
+        {
+            collector.setPower(0);
+        }
+
+        if (gamepad1.dpad_up)
+            winch.setPower(1);
+        else if (gamepad1.dpad_down)
+            winch.setPower(-1);
+        else
+            winch.setPower(0);
+
+        if (gamepad2.dpad_right)
+            collectorWall.setPosition(1);
+        else
+            collectorWall.setPosition(0.6);
+
+
+        spool.setPower(gamepad2.right_stick_x*0.9);
+
+        arm.setPower(gamepad2.left_stick_y * 0.6);
+
+
+        double leftFrontPower;
+        double rightFrontPower;
+        double leftBackPower;
+        double rightBackPower;
+
+        double drive =  gamepad1.left_stick_y * slowModeValue;
+        double turn  =  -gamepad1.right_stick_x;
+        leftFrontPower    = Range.clip(drive + turn, -1.0, 1.0);
+        rightFrontPower   = Range.clip(drive - turn, -1.0, 1.0);
+        leftBackPower     = Range.clip(drive + turn, -1.0, 1.0);
+        rightBackPower    = Range.clip(drive - turn, -1.0, 1.0);
+
+        leftFrontDrive.setPower(leftFrontPower);
+        rightFrontDrive.setPower(rightFrontPower);
+        leftBackDrive.setPower(leftBackPower);
+        rightBackDrive.setPower(rightBackPower);
+
+
+        // Show the elapsed game time and wheel power.
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.addData("Slow-mode: ", isOnA);
+        telemetry.addData("Front Motors", "leftFront (%.2f), rightFront (%.2f), ", leftFrontPower, rightFrontPower);
+        telemetry.addData("Back Motors", "leftBack (%.2f), rightBack (%.2f), ", leftBackPower, rightBackPower);
+        telemetry.addData("Arm: ", arm.getCurrentPosition());
+
+
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/AutonomousSquare.java
@@ -0,0 +1,265 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.Testing;
+
+import com.disnodeteam.dogecv.detectors.roverrukus.GoldAlignDetector;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.ReadWriteFile;
+
+import org.firstinspires.ftc.robotcore.external.Func;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
+import java.io.File;
+import java.util.Locale;
+
+/**
+ * File Created bu Abhiram T. (5518)
+ * Version 1.0
+ * Date: 1/23/2018
+ *
+ * Contents:
+ * Write premature Autonomous to initialize basic operations
+ */
+
+@Autonomous(name="Autonomous Square 1.2", group="Autonomous")
+@Disabled
+public class AutonomousSquare extends LinearOpMode
+{
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+
+    //Motors
+    private DcMotor leftFrontDrive   = null;
+    private DcMotor rightFrontDrive  = null;
+    private DcMotor leftBackDrive    = null;
+    private DcMotor rightBackDrive   = null;
+
+    //Gyro
+    private BNO055IMU imu;
+    private Orientation angles;
+
+    //DogeCV Alignment Detector
+    private GoldAlignDetector GAD = new GoldAlignDetector();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Initialize the hardware variables
+        leftFrontDrive  = hardwareMap.get(DcMotor.class, "1");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "2");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "3");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "0");
+
+        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        /*
+         * Save GyroSensor Calibration Data
+         */
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.loggingEnabled = true;
+        parameters.loggingTag     = "IMU";
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        BNO055IMU.CalibrationData calibrationData = imu.readCalibrationData();
+
+        String filename = "AdafruitIMUCalibration.json";
+        File file = AppUtil.getInstance().getSettingsFile(filename);
+        ReadWriteFile.writeFile(file, calibrationData.serialize());
+        telemetry.log().add("saved to '%s'", filename);
+
+        //Read Calibration Data
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu.initialize(parameters);
+        composeGyroTelemetry();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+        imu.startAccelerationIntegration(new Position(), new Velocity(), 100);
+
+        for (int multiplier=1; multiplier<=4; multiplier++)
+        {
+            if (multiplier == 4)
+                turnToAngle(true, 0.0, 0.35);
+            else
+                turnToAngle(true, 90.0 * multiplier, 0.35);
+
+            powerMotors(0.5);
+            sleep(1000);
+            powerMotors(0.0);
+        }
+
+        for (int multiplier=1; multiplier<=4; multiplier++)
+        {
+            if (multiplier == 4)
+                turnToAngle(false, 0.0, 0.35);
+            else
+                turnToAngle(false, 90.0 * multiplier, 0.35);
+
+            powerMotors(0.5);
+            sleep(1000);
+            powerMotors(0.0);
+        }
+
+        powerMotors(-1.0);
+        sleep(1000);
+        powerMotors(0.0);
+    }
+
+    private void turnToAngle(boolean isRight, double targetAngle, double turnPower)
+    {
+        if (isRight)
+        {
+            telemetry.update();
+            targetAngle = -targetAngle;
+
+            while (!(angles.firstAngle < targetAngle + 5 && angles.firstAngle > targetAngle - 10) && opModeIsActive()) {
+                telemetry.update();
+
+                leftFrontDrive.setPower(turnPower);
+                rightFrontDrive.setPower(-turnPower);
+                leftBackDrive.setPower(turnPower);
+                rightBackDrive.setPower(-turnPower);
+            }
+
+            leftFrontDrive.setPower(0);
+            rightFrontDrive.setPower(0);
+            leftBackDrive.setPower(0);
+            rightBackDrive.setPower(0);
+        }
+        else
+        {
+            telemetry.update();
+
+            while (!(angles.firstAngle < targetAngle + 5 && angles.firstAngle > targetAngle - 10) && opModeIsActive()) {
+                telemetry.update();
+
+                leftFrontDrive.setPower(-turnPower);
+                rightFrontDrive.setPower(turnPower);
+                leftBackDrive.setPower(-turnPower);
+                rightBackDrive.setPower(turnPower);
+            }
+
+            leftFrontDrive.setPower(0);
+            rightFrontDrive.setPower(0);
+            leftBackDrive.setPower(0);
+            rightBackDrive.setPower(0);
+        }
+
+    }
+
+    private void powerMotors(double power)
+    {
+        leftFrontDrive.setPower(power);
+        rightFrontDrive.setPower(power);
+        leftBackDrive.setPower(power);
+        rightBackDrive.setPower(power);
+    }
+
+    private void composeGyroTelemetry()
+    {
+
+        // At the beginning of each telemetry update, grab a bunch of data
+        // from the IMU that we will then display in separate lines.
+        telemetry.addAction(new Runnable() { @Override public void run()
+        {
+            // Acquiring the angles is relatively expensive; we don't want
+            // to do that in each of the three items that need that info, as that's
+            // three times the necessary expense.
+            angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        }
+        });
+
+        telemetry.addLine()
+                .addData("status", new Func<String>() {
+                    @Override public String value() {
+                        return imu.getSystemStatus().toShortString();
+                    }
+                })
+                .addData("calib", new Func<String>() {
+                    @Override public String value() {
+                        return imu.getCalibrationStatus().toString();
+                    }
+                });
+
+        telemetry.addLine()
+                .addData("heading", new Func<String>() {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.firstAngle);
+                    }
+                })
+                .addData("roll", new Func<String>() {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.secondAngle);
+                    }
+                })
+                .addData("pitch", new Func<String>() {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.thirdAngle);
+                    }
+                });
+    }
+
+    private String formatAngle(AngleUnit angleUnit, double angle)
+    {
+        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
+    }
+
+    private String formatDegrees(double degrees)
+    {
+        return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
+    }
+}
